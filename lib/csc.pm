010cd75ab622f51134e32e768a2a7d1612f552f6
Daniel Tiefnig <dtiefnig@sipwise.com>
2008-08-22 10:17:10 +0000

    creating new branch 1.0 from revision 712

--- lib/csc.pm
+++ lib/csc.pm
@@ -21,7 +21,7 @@ use Catalyst qw/ConfigLoader Static::Simple
                 Session Session::Store::FastMmap Session::State::Cookie
                /;
 
-our $VERSION = '0.01';
+our $VERSION = '1.0';
 
 # Configure the application. 
 #
