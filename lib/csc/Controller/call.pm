a1a57da282022da349199b9ef323f71a92f8bef8
Andreas Granig <agranig@sipwise.com>
2010-04-07 16:42:35 +0000

    Changed click2dial to use ossbss API.

--- lib/csc/Controller/call.pm
+++ lib/csc/Controller/call.pm
@@ -42,84 +42,73 @@ both of them.
 sub click2dial : Local {
     my ( $self, $c ) = @_;
 
-	my ($callee_user, $callee_domain);
-	my ($caller_user, $caller_domain, $caller_pass);
+    my $announce_file = "click2dial";
+    my %calldata = ();
 
-	my $proxy = "192.168.101.162";
-	my $xmlrpc_url = "http://192.168.102.162:8090";
-	my $announce_file = "click2dial";
+    my ($callee_user, $callee_domain);
+    my $callee = $c->request->param('d');
+    if($callee =~ /^\+?\d+$/)
+    {
+        if($callee =~ /^0[1-9][0-9]+$/)
+        {
+            $callee =~ s/^0//;
+            $callee = "00" . $c->session->{user}{data}{cc} . $callee;
+        }
+        elsif($callee =~ /^00[1-9][0-9]+$/)
+        {
+            # we're fine already
+        }
+        elsif($callee =~ /^\+[1-9][0-9]+$/)
+        {
+            $callee =~ s/^\+/00/;
+        }
+        elsif($callee =~ /^[1-9][0-9]+$/)
+        {
+            $callee = "00" . $c->session->{user}{data}{cc} . 
+            $c->session->{user}{data}{ac} . $callee;
+        }
+        else
+        {
+            $c->log->error('***call::click2dial with invalid callee ' . $callee);
 
-	my $callee = $c->request->param('d');
-	if($callee =~ /^\+?\d+$/)
-	{
-		if($callee =~ /^0[1-9][0-9]+$/)
-		{
-			$callee =~ s/^0//;
-			$callee = "00" . $c->session->{user}{data}{cc} . $callee;
-		}
-		elsif($callee =~ /^00[1-9][0-9]+$/)
-		{
-			# we're fine already
-		}
-		elsif($callee =~ /^\+[1-9][0-9]+$/)
-		{
-			$callee =~ s/^\+/00/;
-		}
-		elsif($callee =~ /^[1-9][0-9]+$/)
-		{
-			$callee = "00" . $c->session->{user}{data}{cc} . 
-				$c->session->{user}{data}{ac} . $callee;
-		}
-		else
-		{
-			$c->log->error('***call::click2dial with invalid callee ' . $callee);
+            # TODO: Error handling, invalid uri or number
+            $c->response->redirect($c->uri_for('/desktop'));
+            return;
+        }
+        $callee_user = $callee;
+        $callee_domain = $c->session->{user}{data}{domain};
+    }
+    else
+    {
+        $callee =~ s/^sip://;
+        $callee =~ s/:\d+(;.+)?//; # strip uri port and params like "sip:foo@bar.com:5060;line=xy"
+        ($callee_user, $callee_domain) = $callee =~ /^(.+)@(.+)$/;
+        unless(defined $callee_user && defined $callee_domain)
+        {
+            $c->log->error('***call::click2dial with invalid callee ' . $callee);
 
-			# TODO: Error handling, invalid uri or number
-			$c->response->redirect($c->uri_for('/desktop'));
-			return;
-		}
-		$callee_user = $callee;
-		$callee_domain = $c->session->{user}{data}{domain};
-	}
-	else
-	{
-		$callee =~ s/^sip://;
-		$callee =~ s/:\d+(;.+)?//; # strip uri port and params like "sip:foo@bar.com:5060;line=xy"
-		($callee_user, $callee_domain) = $callee =~ /^(.+)@(.+)$/;
-		unless(defined $callee_user && defined $callee_domain)
-		{
-			$c->log->error('***call::click2dial with invalid callee ' . $callee);
+            # TODO: Error handling, invalid uri or number
+            $c->response->redirect($c->uri_for('/desktop'));
+            return;
+        }
+    }
 
-			# TODO: Error handling, invalid uri or number
-			$c->response->redirect($c->uri_for('/desktop'));
-			return;
-		}
-	}
+    $calldata{username} = $c->session->{user}{username};
+    $calldata{domain} = $c->session->{user}{domain};
+    $calldata{callee_user} = $callee_user;
+    $calldata{callee_domain} = $callee_domain;
+    $calldata{caller_user} =  $c->session->{user}{data}{username};
+    $calldata{caller_domain} = $c->session->{user}{data}{domain};
+    $calldata{caller_pass} =  $c->session->{user}{data}{password};
+    $calldata{announcement} =  $announce_file;
 
-        ## dtiefnig 08.01.2008 - should use SIP user and pass, shouldn't it?
-        ## $c->session->{user}{username} now is webusername!
-	# $caller_user =  $c->session->{user}{username};
-	# $caller_domain = $c->session->{user}{domain};
-	# $caller_pass =  $c->session->{user}{password};
-	$caller_user =  $c->session->{user}{data}{username};
-	$caller_domain = $c->session->{user}{data}{domain};
-	$caller_pass =  $c->session->{user}{data}{password};
-	
-	$c->log->error('***call::click2dial with caller ' . $caller_user . ':' . $caller_pass . '@' . $caller_domain);
-	$c->log->error('***call::click2dial with callee ' . $callee_user . '@' . $callee_domain);
+    return 1 unless $c->model('Provisioning')->call_prov($c, 'voip', 'dial_out',
+                                                         \%calldata
+                                                        );
 
-	my $cli = RPC::XML::Client->new($xmlrpc_url);
-	my $resp = $cli->send_request('dial_auth_b2b', 'click2dial', $announce_file,
-		'sip:'.$caller_user.'@'.$caller_domain,
-		'sip:'.$callee_user.'@'.$callee_domain,
-		'sip:'.$caller_user.'@'.$proxy.';sw_domain='.$caller_domain,
-		'sip:'.$callee_user.'@'.$proxy.';sw_domain='.$callee_domain,
-		$caller_domain, $caller_user, $caller_pass);
-	
-	# TODO: Error handling, check if XMLRPC was successful, and if not, try antoher proxy?
-	
-	$c->response->redirect($c->request->referer);
-	return;
+
+    $c->response->redirect($c->request->referer);
+    return;
 }
 
 =head1 BUGS AND LIMITATIONS
